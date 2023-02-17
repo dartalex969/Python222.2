1d47c5dc04283c673fde62b03f9a86cb0a740ee2
kimskiy903gmail.com <kimskiy903@gmail.com>
2023-02-17 21:59:40 +0500

    сделал функцию удаления

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ with open('test.json') as json_file:
     print(employees)
 
 
-
-
-
-
-
-
-
-
+def deleting(data):
+    surname = input('Введите фамилию сотрудника, которого хотите удалить: ')
+    name = input('Далее имя сотрудника: ')
+    age = int(input('И теперь возраст сотрудника: '))
+    for i in employees:
+        if i['surname'] == surname and i['name'] == name and i['age'] == age:
+            employees.pop(i)
